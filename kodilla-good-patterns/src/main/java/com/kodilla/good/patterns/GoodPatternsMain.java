9f41145e9ca24e4f089f9ca70b3bc63e21586592
Niedamir <rafallewandowski09@gmail.com>
2019-06-04 11:29:11 +0200

    module 13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/GoodPatternsMain.java
@@ -0,0 +1,11 @@
+package com.kodilla.good.patterns;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+public class GoodPatternsMain {
+	public static void main(String[] args) {
+		MovieStore module13_1 = new MovieStore();
+
+		module13_1.moviesStream();
+	}
+}
